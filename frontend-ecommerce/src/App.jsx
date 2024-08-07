22637ae243cdbe7fd17e02944d9735ca34b38c82
ThiagoSaraceni <thiagorx123@gmail.com>
2024-08-06 21:15:59 -0300

    first commit

---
+++ frontend-ecommerce/src/App.jsx
@@ -0,0 +1,14 @@
+import { Navbar } from "./components/layout/navbar/navbar";
+import { Home } from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <h1>oi</h1>
+      <Home />
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
