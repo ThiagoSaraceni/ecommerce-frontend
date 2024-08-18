3eb416930a006292ed451fbd23151d54da6fdf16
ThiagoSaraceni <thiagorx123@gmail.com>
2024-08-18 12:13:26 -0300

    feat: transicao para abrir card de filtros

--- frontend-ecommerce/src/components/layout/filter/index.jsx
+++ frontend-ecommerce/src/components/layout/filter/index.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { ArrowDownIcon } from "./ArrowDownIcon";
+import { useState } from "react";
 
 const TextFilter = styled.h6`
   font-family: "Saira", sans-serif;
@@ -22,30 +23,39 @@ const DColumn = styled.div`
 `;
 
 const Card = styled.div`
+  max-height: ${(props) => (props.isOpen ? "200px" : "0px")};
+  opacity: ${(props) => (props.isOpen ? "1" : "0")};
+  overflow: hidden;
   background-color: #ffff;
-  padding: 16px;
+  padding: ${(props) => (props.isOpen ? "16px" : "0 16px")};
   border-radius: 4px;
-  box-shadow: 0px 4px 12px 0px rgba(000000);
+  box-shadow: 0px 4px 12px 0px rgba(0, 0, 0, 0.1);
+  transition: max-height 0.7s linear, opacity 0.7s linear, padding 0.7s linear;
+
   li {
     list-style-type: none;
     font-family: "Saira", sans-serif;
     color: #737380;
     cursor: pointer;
   }
+
   li + li {
     margin-top: 4px;
   }
+
+  width: 100%;
 `;
 
 export const FilterBy = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <DColumn>
-        <Dflex>
+        <Dflex onClick={() => setIsOpen(!isOpen)}>
           <TextFilter>Organizar por</TextFilter>
           <ArrowDownIcon />
         </Dflex>
-        <Card>
+        <Card isOpen={isOpen}>
           <ul>
             <li>Novidades</li>
             <li>Preço: Maior - menor</li>
