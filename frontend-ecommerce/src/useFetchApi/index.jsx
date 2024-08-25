8acb98e502483bd2c85e61661647664337cd6b60
ThiagoSaraceni <thiagorx123@gmail.com>
2024-08-24 23:49:50 -0300

    feat: adicionando filtragem

--- frontend-ecommerce/src/useFetchApi/index.jsx
+++ frontend-ecommerce/src/useFetchApi/index.jsx
@@ -1,44 +1,45 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 export const URLAPIECOMMERCE = "http://localhost:3000/api/";
 
-export const useFetchApi = (url, params = {}) => {
+export const useFetchApi = (baseUrl, params = {}, options = {}) => {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchData = async () => {
-    setIsLoading(true);
-    setError(null);
-
-    try {
-      const response = await fetch(url, {
-        method: params?.method || "GET",
-        headers: {
-          "Content-Type": "application/json",
-          ...params?.headers,
-        },
-        body: params?.body ? JSON.stringify(params?.body) : null,
-      });
-
-      if (!response.ok) {
-        throw new Error(`Erro ${response.status} - ${response.statusText}`);
-      }
-
-      const result = await response.json();
-      setData(result);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  const memoizedParams = useMemo(() => params, [JSON.stringify(params)]);
+  const memoizedBaseUrl = useMemo(() => baseUrl, [baseUrl]);
 
   useEffect(() => {
-    if (url) {
-      fetchData();
-    }
-  }, [url]);
+    const queryString = new URLSearchParams(params).toString();
+    const url = `${baseUrl}?${queryString}`;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, {
+          method: options?.method || "GET",
+          headers: {
+            "Content-Type": "application/json",
+            ...options?.headers,
+          },
+          body: options?.body ? JSON.stringify(options?.body) : null,
+        });
+
+        if (!response.ok) {
+          throw new Error(`Erro ${response.status} - ${response.statusText}`);
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [memoizedBaseUrl, memoizedParams]);
 
   return { data, isLoading, error };
 };
